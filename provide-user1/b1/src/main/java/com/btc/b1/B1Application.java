b32ff2609ff5ee98e67eb5830779886fb3b85cad
AkinaHE <akinahe0704@126.com>
2018-12-16 16:42:42 +0800

    b1

---
+++ provide-user1/b1/src/main/java/com/btc/b1/B1Application.java
@@ -0,0 +1,17 @@
+package com.btc.b1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class B1Application    {
+
+    public static void main(String[] args) {
+        SpringApplication.run(B1Application.class, args);
+    }
+
+
+}
+
---
+++ provide-user1/b1/src/main/java/com/btc/b1/service/TestService.java
@@ -0,0 +1,8 @@
+package com.btc.b1.service;
+
+/**
+ * 接口
+ */
+public interface TestService {
+   public String hello();
+}
